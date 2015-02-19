de19b61c522762f5d4ec6e67160ccf68085f64ef
Nick Rafter <nicholas.rafter@gmail.com>
2015-02-19 12:01:46 -0500

    Exposed full http request to filter function for more configurable filtering

--- examples/express-embed/server.js
+++ examples/express-embed/server.js
@@ -4,9 +4,20 @@ var Server = require('../..').Server
 var express = require('express')
 var app = express()
 
+// https://wiki.theory.org/BitTorrentSpecification#peer_id
+var whitelist = {
+  UT: true // uTorrent
+}
+
 var server = new Server({
   http: false, // we do our own
-  udp: false   // not interested
+  udp: false,  // not interested
+  filter: function (params) {
+    // black/whitelist for disallowing/allowing specific clients [default=allow all]
+    // this example only allows the uTorrent client
+    var client = params.peer_id[1] + params.peer_id[2]
+    return whitelist[client]
+  }
 })
 
 var onHttpRequest = server.onHttpRequest.bind(server)
